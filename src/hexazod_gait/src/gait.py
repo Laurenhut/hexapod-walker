a947dd4258e09c78012585950514a83cfabfc3b3
laurenhut <l.hutson@live.com>
2019-04-28 18:27:02 -0500

    add multi leg ik calculation and loading of different gait styles

--- src/hexazod_gait/src/gait.py
+++ src/hexazod_gait/src/gait.py
@@ -3,6 +3,8 @@ import rospy
 import sys
 import numpy
 import math
+import yaml
+
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
 
@@ -42,13 +44,33 @@ class HexazodGait(object):
         self.coxa = 3.75  # 1 gamma
         # self.calculate_angles(x, y, z_offset, 1)
 
-    def calculate_angles(self, x, y, z_offset, leg):
+    def calculate_for_all_legs(self, gait_selection):
+        # make a dictionary with all the available walking gaits
+        # when a gait is chosen calculate angles for all legs for that gait, 
+        # gait_name: (first frame)[(leg1 pos)[x,y,z], (leg2 pos)[], etc...], (2nd frame)[]
+        # calculated angles = [(leg1)[gamma,alpha,beta], (leg2)[gamma,alpha,beta], etc...]
+        # joint angles should be obtained from a dictionary inside of a config file. all of these should be predeturmined
+        try:
+            with open("/../config/gaits.yaml",'r') as stream: 
+                gaits = yaml.safe_load(stream)
+            print gaits
+        except: 
+            raise "Error: cannot open gaits file"
+
+        calculated_angles = []
+        for frame in gaits[gait_selection]:
+            for numbered_leg in frame: 
+                calculated_angles.append(self.calculate_angles(numbered_leg[0], numbered_leg[1], numbered_leg[2]))
+            self.send_values(calculated_angles)
+
+
+    def calculate_angles(self, x, y, z_offset):
         """
         calculates gamma
         out = tha alpha beta gamma of one leg
         in = the desired coordinate (x,y,z) of a leg(int), nubered leg (int)
         """
-        leg_values = {"leg_num": leg, "gamma": 0, "alpha": 0, "beta": 0}
+        leg_values = [] #{"leg_num": leg, "gamma": 0, "alpha": 0, "beta": 0}
 
         g = math.hypot(x, y)
         print(g)
@@ -75,26 +97,23 @@ class HexazodGait(object):
 
         print("gamma: %s" % ((gamma*180)/ math.pi))
 
-        leg_values["gamma 1"] = gamma
-        leg_values["alpha 2"] = alpha
-        leg_values["beta 3"] = beta
-        print(leg_values)
-        self.send_values(leg_values)
+        leg_values.append(gamma)
+        leg_values.append(alpha)
+        leg_values.append(beta)
+
+        return leg_values
 
 
-    def send_values(self, leg_values):
+    def send_values(self, leg_angles):
         print("in send values")
-        position = [0.0]*18
+        position = []
         rate = rospy.Rate(50) # 10hz
-        leg_num = leg_values["leg_num"]*3
-        for x in range((leg_num-3), leg_num): #to save in right position
-            for i in range(1,3):
-                for keys in leg_values.keys():
-                    str_i = str(i)
-                    if str_i in keys:
-                        print("found %s " % keys)
-                        position[x] = leg_values[keys]
 
+        for legs in leg_angles: #iterates through the list to save joint angles in the right position
+            for angle in legs:
+                position.append(angle)
+
+        # publishes the joint angles
         for i in range(100):
             print("pub %i" %i)
             gait_state = JointState()
@@ -119,8 +138,9 @@ def main():
     x = 6
     #zero out
     this = HexazodGait()
-    this.calculate_angles(x, y, z_offset, 1)
+    this. calculate_for_all_legs("triangle")
     rospy.spin()
+
 if __name__ == '__main__':
     try:
 	    main()
